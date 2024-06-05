f6ceb55b06f097fa882df67f31c74504bf987bfb
Someguy151 <jonathanhooper3@hotmail.com>
2024-06-04 17:55:58 -0700

    Fixed and improved HTML Handler and HTMLFormatted

--- bin/HTMLFormatted.pl
+++ bin/HTMLFormatted.pl
@@ -4,8 +4,57 @@ use strict;
 use warnings;
 use FindBin;
 use lib "$FindBin::Bin/../lib";
-# use MyApp::HTMLHandler;
+use MyApp::HTMLHandler;
+use Getopt::Long;
 
 # Determine base directory
 my $base_dir = "$FindBin::Bin/..";
 
+# Command-line options
+my $output_html = "$base_dir/Outputs/output_html.html";
+GetOptions("output=s" => \$output_html) or die "Error in command-line arguments\n";
+
+# Create HTMLHandler object
+my $msg = MyApp::HTMLHandler->new();
+
+# Add content
+eval {
+    $msg->add_header("Test");
+    $msg->add_msg("Simple test here.");
+    $msg->add_msg_color("b", "red", "Bold text in red.");
+    $msg->add_msg_color("i", "black", "Italic text in black.");
+    $msg->add_msg_color("del", "cyan", "Strikethrough text in cyan.");
+    $msg->add_msg_color("small", "blue", "Small text in blue.");
+    $msg->add_msg_color("underline", "green", "Underlined text in green.");
+
+    $msg->add_break();
+
+    $msg->start_list("numbered");
+    $msg->add_to_list("item 1");
+    $msg->add_to_list("item 2");
+    $msg->end_list();
+
+    $msg->start_list("bulleted");
+    $msg->add_to_list("item 1");
+    $msg->add_to_list("item 2");
+    $msg->end_list();
+
+    $msg->start_table();
+    $msg->add_row("Head1", "Head2");
+    $msg->add_row("one", "two");
+    $msg->end_table();
+
+    $msg->add_email("Email me at: ", "jonathan.hooper@ XYZ.com");
+    $msg->add_break();  # Add a break between the email link and the next link
+    $msg->add_link("Just duck it: ", "https://start.duckduckgo.com/");
+};
+if ($@) {
+    die "An error occurred while generating the HTML content: $@\n";
+}
+
+# Write to file
+open(my $output_fh, '>', $output_html) or die "Cannot open $output_html for writing: $!\n";
+print $output_fh $msg->get_full_message();
+close $output_fh;
+
+print "HTML file generated successfully: $output_html\n";
--- lib/myApp/HTMLHandler.pm
+++ lib/myApp/HTMLHandler.pm
@@ -2,14 +2,12 @@ package MyApp::HTMLHandler;
 
 use strict;
 use warnings;
-use File::Slurp;
-
 
 sub new {
-    my $class = shift @_;
+    my $class = shift;
     my $self = {
-        _message=>" ",
-        _state=>'normal'
+        _message => "",
+        _state => 'normal',
     };
     bless $self, $class;
     return $self;
@@ -17,43 +15,44 @@ sub new {
 
 sub add_header {
     my ($self, $hdr) = @_;
-    $self->{_message} .= "<h1>".$hdr."</h1>\n";
+    $self->{_message} .= "<h1>" . _escape_html($hdr) . "</h1>\n";
 }
 
 sub add_msg {
     my ($self, $msg) = @_;
-    $self->{_message} .= "<p>".$msg."</p>\n";
+    $self->{_message} .= "<p>" . _escape_html($msg) . "</p>\n";
 }
 
 sub add_email {
     my ($self, $name, $addr) = @_;
-    $self->{_message} .= '<a href="mailto:'.$addr.'">'.$name."</a>\n";
+    $self->{_message} .= '<a href="mailto:' . _escape_html($addr) . '">' . _escape_html($name) . "</a>\n";
 }
 
 sub add_link {
     my ($self, $name, $link) = @_;
-    $self->{_message} .= '<a href="mailto:'.$link.'">'.$name."</a>\n";
+    $self->{_message} .= '<a href="' . _escape_html($link) . '">' . _escape_html($name) . "</a>\n";
 }
 
 sub add_msg_color {
     my ($self, $attr, $color, $msg) = @_;
-    $self->{_message} .= "<p>\n";
+    my %valid_attrs = map { $_ => 1 } qw(b i u del small);
     if ($attr eq 'underline') {
-        $self->{_message} .= '<span style="text-decoration:underline:color:'.$color.'">'.$msg."</span>\n";
-    } else {
-        $self->{_message} .= '<span style="color:'.$color.'"><'.$attr.'>'.$msg.'</'.$attr."></span>\n";
+        $attr = 'u'; # Translate 'underline' to 'u'
     }
-    $self->{_message} .= "</p>\n";
+    if (!$valid_attrs{$attr}) {
+        die "Invalid attribute: $attr";
+    }
+    $self->{_message} .= "<p><span style=\"color:$color\"><$attr>" . _escape_html($msg) . "</$attr></span></p>\n";
 }
 
 sub add_break {
-    my ($self, $msg) = @_;
-    $self->{_message} .= '<br />'."\n";
+    my $self = shift;
+    $self->{_message} .= '<br />' . "\n";
 }
 
 sub start_list {
     my ($self, $type) = @_;
-    if ($type =~ /number/) {
+    if ($type eq 'numbered') {
         $self->{_message} .= '<ol>';
         $self->{_state} = 'numberlist';
     } else {
@@ -63,54 +62,54 @@ sub start_list {
 }
 
 sub end_list {
-    my ($self, $type) = @_;
+    my $self = shift;
     if ($self->{_state} eq 'numberlist') {
         $self->{_message} .= '</ol>';
-        $self->{_state} = 'normal';
     } elsif ($self->{_state} eq 'bulletlist') {
         $self->{_message} .= '</ul>';
-        $self->{_state} = 'normal';
     }
+    $self->{_state} = 'normal';
 }
 
 sub add_to_list {
     my ($self, $msg) = @_;
-    $self->{_message} .= "<li>".$msg."<mail::/li>\n";
+    $self->{_message} .= "<li>" . _escape_html($msg) . "</li>\n";
 }
 
 sub start_table {
-    my ($self, $msg) = @_;
-    $self->{_message} .= '<table border="1">'."\n";
+    my $self = shift;
+    $self->{_message} .= '<table border="1">' . "\n";
     $self->{_state} = "table";
 }
 
-sub add_first_row {
-    my ($self, @msg) = @_;
-    $self->{_message} .= "<tr>\n";
-    foreach my $cell (@msg) {
-        $self->{_message} .= "<td bgcolor=orange><strong><font size=4px>".$cell."</font></strong></td>\n";    
-    }
-    $self->{_message} .= "</tr>\n";
-}
-
 sub add_row {
-    my ($self, @msg) = @_;
+    my ($self, @cells) = @_;
     $self->{_message} .= "<tr>\n";
-    foreach my $cell (@msg) {
-        $self->{_message} .= "<td>".$cell."</td>\n";    
+    foreach my $cell (@cells) {
+        $self->{_message} .= "<td>" . _escape_html($cell) . "</td>\n";
     }
     $self->{_message} .= "</tr>\n";
 }
 
 sub end_table {
-    my ($self, @msg) = @_;
-    if ($self->{_state} ne 'table') {
-        # $error_out("Not in table mode.");
-    }
-    $self->{_message} .= "</table>\n";
+    my $self = shift;
+    $self->{_message} .= "</table>\n" if $self->{_state} eq 'table';
+    $self->{_state} = 'normal';
 }
 
 sub get_full_message {
-    my ($self, @msg) = @_;
-    return $self->{_message} .= "<html><body>\n".$self->{_message}."\n</html></body>\n";
+    my $self = shift;
+    return "<html><body>\n" . $self->{_message} . "\n</body></html>\n";
 }
+
+sub _escape_html {
+    my $text = shift;
+    $text =~ s/&/&amp;/g;
+    $text =~ s/</&lt;/g;
+    $text =~ s/>/&gt;/g;
+    $text =~ s/"/&quot;/g;
+    $text =~ s/'/&#39;/g;
+    return $text;
+}
+
+1;  # End of MyApp::HTMLHandler
